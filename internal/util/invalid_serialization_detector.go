18eb33e388e98b2b3a9096125f90acf5e21fafde
Jared O'Connell <joconnel@redhat.com>
2024-02-21 18:58:46 -0500

    Added documentation to satisfy linter

--- internal/util/invalid_serialization_detector.go
+++ internal/util/invalid_serialization_detector.go
@@ -17,6 +17,8 @@ func NewInvalidSerializationDetectorSchema() *InvalidSerializationDetectorSchema
 // current operation, then that indicates double serialization or double unserialization.
 type InvalidSerializationDetectorSchema struct{}
 
+// Unserialize unserializes the data. In this schema that means checking for an
+// invalid state and returning "unserialized".
 func (d InvalidSerializationDetectorSchema) Unserialize(data any) (any, error) {
 	// The input is expected to always be a string.
 	// If the input is equal to "unserialized", then that means it's being unserialized a second time.
@@ -41,6 +43,7 @@ func (d InvalidSerializationDetectorSchema) detectInvalidValue(operation string,
 	return operation, nil
 }
 
+// UnserializeType is a string-output-typed version of Unserialize.
 func (d InvalidSerializationDetectorSchema) UnserializeType(data any) (string, error) {
 	unserialized, err := d.Unserialize(data)
 	if err != nil {
@@ -49,36 +52,47 @@ func (d InvalidSerializationDetectorSchema) UnserializeType(data any) (string, e
 	return unserialized.(string), nil
 }
 
+// ValidateCompatibility ensures that the input data or schema is compatible with
+// the given InvalidSerializationDetectorSchema.
 func (d InvalidSerializationDetectorSchema) ValidateCompatibility(_ any) error {
 	// Not applicable to this data type
 	return nil
 }
 
+// Validate ensures that the data can be serialized.
 func (d InvalidSerializationDetectorSchema) Validate(data any) error {
 	_, err := d.Serialize(data)
 	return err
 }
 
+// ValidateType is a string-input typed version of Validate.
 func (d InvalidSerializationDetectorSchema) ValidateType(data string) error {
 	return d.Validate(data)
 }
 
+// Serialize serializes the data. In this schema that means checking for an
+// invalid state, and returning "serialized".
 func (d InvalidSerializationDetectorSchema) Serialize(data any) (any, error) {
 	// The input is expected to always be a string.
 	// If the input is equal to "serialized", then that means it's being serialized a second time.
 	return d.detectInvalidValue("serialized", data)
 }
 
+// SerializeType is string-input-typed version of Serialize.
 func (d InvalidSerializationDetectorSchema) SerializeType(data string) (any, error) {
 	return d.Serialize(data)
 }
 
-func (d InvalidSerializationDetectorSchema) ApplyScope(scope schema.Scope) {}
+// ApplyScope is for applying a scope to the references. Does not apply to this object.
+func (d InvalidSerializationDetectorSchema) ApplyScope(_ schema.Scope) {}
 
+// TypeID returns the category of type this type is. Returns string because
+// the valid states of this type include all strings.
 func (d InvalidSerializationDetectorSchema) TypeID() schema.TypeID {
 	return schema.TypeIDString // This is a subset of a string schema.
 }
 
+// ReflectedType returns the reflect.Type for a string.
 func (d InvalidSerializationDetectorSchema) ReflectedType() reflect.Type {
 	return reflect.TypeOf("")
 }
