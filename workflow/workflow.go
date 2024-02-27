b3e28cb78df0ede16d3125ce19a14b5c14aced24
Matthew F Leader <mleader@redhat.com>
2024-02-27 12:41:27 -0500

    rm validation

--- workflow/workflow.go
+++ workflow/workflow.go
@@ -67,10 +67,6 @@ func (e *executableWorkflow) Execute(ctx context.Context, serializedInput any) (
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to reserialize workflow input (%w)", err)
 	}
-	err = e.input.Validate(reSerializedInput)
-	if err != nil {
-		return "", nil, fmt.Errorf("bug: reserialized data is invalid %v", reSerializedInput)
-	}
 
 	// We use an internal cancel function to abort the workflow if something bad happens.
 	ctx, cancel := context.WithCancel(ctx)
