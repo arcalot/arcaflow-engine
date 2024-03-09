9b4f1b4927995d3cc87354c84b8d579f7c112f31
Matthew F Leader <mleader@redhat.com>
2024-03-09 15:38:25 -0500

    unsure of how to test for equality

--- workflow/yaml_test.go
+++ workflow/yaml_test.go
@@ -4,6 +4,7 @@ import (
 	"go.arcalot.io/assert"
 	"go.arcalot.io/log/v2"
 	"go.flow.arcalot.io/engine/workflow"
+	"go.flow.arcalot.io/pluginsdk/schema"
 	"testing"
 )
 
@@ -42,6 +43,21 @@ func Test_SchemaWorkflow(t *testing.T) {
 	logger := log.NewLogger(log.LevelDebug, log.NewTestWriter(t))
 	stepRegistry := NewTestImplStepRegistry(logger, t)
 	yamlConverter := workflow.NewYAMLConverter(stepRegistry)
-	_, err := yamlConverter.FromYAML([]byte(fullWorkflow))
+	wf, err := yamlConverter.FromYAML([]byte(fullWorkflow))
 	assert.NoError(t, err)
+
+	outputIDExp := "success"
+	outputSchemaRootID := "RootObjectOut"
+	outputSchemaProperties := map[string]*schema.PropertySchema{
+		"message": schema.NewPropertySchema(
+			schema.NewStringSchema(schema.IntPointer(1), nil, nil),
+			nil, false, nil, nil,
+			nil, nil, nil)}
+	rootObjectOut := schema.NewObjectSchema(outputSchemaRootID, outputSchemaProperties)
+	stepOutputSchema := schema.NewStepOutputSchema(schema.NewScopeSchema(
+		rootObjectOut), nil, false)
+	workflowOutputSchema := map[string]*schema.StepOutputSchema{
+		outputIDExp: stepOutputSchema,
+	}
+	assert.NoError(t, wf.OutputSchema["success"].ValidateCompatibility(workflowOutputSchema["success"]))
 }
