37acaf20a184170cb39fcf914f61f9f11958fca5
Tullio Sebastiani <tsebasti@redhat.com>
2024-06-04 19:04:36 +0200

    adapts the python module to the new way of managing paths on the workflow/config/input files

--- python/arcaflow/arcaflow.py
+++ python/arcaflow/arcaflow.py
@@ -1,39 +1,39 @@
 import os, sys, subprocess, platform
 from .engineargs import EngineArgs
 
+
 def run(arg: EngineArgs) -> int:
     args = []
-    if arg.config != None:
-        if os.path.isfile(arg.config):
-            args.extend(["-config",arg.config])
+    if arg.config is not None:
+        if os.path.isfile("{}/{}".format(arg.context, arg.config)):
+            args.extend(["-config", arg.config])
         else:
             raise Exception("Error: config file {} not found".format(arg.config))
 
-    if arg.input != None:
+    if arg.input is not None:
         if os.path.isfile(arg.input):
-            args.extend(["-input",arg.input])
+            args.extend(["-input", arg.input])
         else:
             raise Exception("Error: input file {} not found".format(arg.input))
-    
-    if arg.context != None:
+
+    if arg.context is not None:
         if os.path.isdir(arg.context):
-            args.extend(["-context",arg.context])
+            args.extend(["-context", arg.context])
         else:
             raise Exception("Error: context path {} not found".format(arg.context))
-    
-    if arg.workflow != None:
-        if os.path.isfile(arg.workflow):
-            args.extend(["-workflow",arg.workflow])
+
+    if arg.workflow is not None:
+        if os.path.isfile("{}/{}".format(arg.context, arg.workflow)):
+            args.extend(["-workflow", arg.workflow])
         else:
             raise Exception("Error: context file {} not found".format(arg.workflow))
 
-    
     is_windows = any(platform.win32_ver())
-    binary_name="arcaflow"
+    binary_name = "arcaflow"
     if is_windows:
-        binary_name="arcaflow.exe"
+        binary_name = "arcaflow.exe"
     return subprocess.call([
-        os.path.join(os.path.dirname(__file__), "bin",binary_name),
+        os.path.join(os.path.dirname(__file__), "bin", binary_name),
         *args
     ])
 
